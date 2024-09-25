568e9f9230e4d6f38ddfa2a633cfdc7282d16c9a
hoshi <chlehdrb4967@gmail.com>
2024-09-25 17:57:54 +0900

    Fix

--- Text RPG/Text RPG/Program.cs
+++ Text RPG/Text RPG/Program.cs
@@ -487,7 +487,7 @@ namespace Text_RPG
                 
                 Random rand = new Random();
                 double a = ((double)rand.Next(100, 201) / 1000) * Atk ;
-                // (Atk~Atk*2)중 랜덤으로 숫자를 뽑아서 100으로 나눠준 결과?
+                
 
                 
 
